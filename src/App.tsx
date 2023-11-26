80de7ca9b0969ffd8fae53eab3603ae6a37f51b8
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-11-26 22:04:13 +0100

    quete react 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 import MyTitle from "./components/MyTitle"
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
     <div>
       <MyTitle />
+      <PokemonCard />
+
     </div>
   );
 }
