73d89e51cf625798335f9e10a630e7e92145c11a
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-11-27 15:48:06 +0100

    Quete React Basic 3

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,43 @@
 function Pokemoncard(){
+
+    const pokemon = pokemonList[0];
+
     return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+        <img src= {pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+            <p>???</p>
+        )}
+        
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
     )
   }
+  
+
+
+  const pokemonList = [
+
+    {
+  
+      name: "bulbasaur",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
+    },
+  
+    {
+  
+      name: "mew",
+  
+    },
+  
+  ];
+
+
+
   
   export default Pokemoncard;
